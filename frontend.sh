e7c69587b9cb3306b993620d7347b75f6046fad3
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-12 17:35:57 +0000

    frontend shellscript

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-script_location=$(pwd)
+script_location=$(pwd) # We are trying to determine the current location using the pwd command
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -7,5 +7,5 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf # As we need to copy the files from current location to the prescribed location, we are declaring the pwd command.
 systemctl restart nginx
--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,7 @@
+script-location=$(pwd)
+
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo # We are copying the .repo file to the yum repo
+
 yum install mongodb-org -y
 
 systemctl enable mongod
