29d97ac833ddd4ffaddd1dfb9615319fecbf232b
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-15 12:07:06 +0000

    front end

--- frontend.sh
+++ frontend.sh
@@ -4,24 +4,72 @@ log=/tmp/roboshop.log #Assigning the log file to direct all the logs.
 echo -e "\e[32m Installing Nginx \e[0m"
 yum install nginx -y | bash &>>${log}
 
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
+
 echo -e "\e[32m Removing default files \e[0m"
 rm -rf /usr/share/nginx/html/* | bash &>>${log}
 
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
+
 echo -e "\e[32m Downloading frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip | bash &>>${log}
+
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
 
 echo -e "\e[32m Creating Nginx directory \e[0m"
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html | bash &>>${log}
 
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
+
 echo -e "\e[32m Extracting the content \e[0m"
 unzip /tmp/frontend.zip | bash &>>${log}
+
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
 
 echo -e "\e[32m Copying the conf file to the default location \e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf | bash &>>${log} # As we need to copy the files from current location to the prescribed location, we are declaring the pwd command.
 
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
+
 echo -e "\e[32m Enabling Nginx \e[0m"
 systemctl enable nginx | bash &>>${log}
 
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
+
 echo -e "\e[32m Starting Nginx \e[0m"
 systemctl restart nginx | bash &>>${log}
+
+if [ $? -eq 0 ]; then
+  echo Successfull
+   else
+  echo Failure
+fi
