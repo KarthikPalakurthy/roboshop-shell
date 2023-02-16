4537114f51d579ef1fce85d023d37672c1df0650
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 11:34:36 +0000

    front end

--- frontend.sh
+++ frontend.sh
@@ -7,30 +7,30 @@ yum install nginx -y &>>${log}
 status_check
 
 print_head "Removing default files"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 status_check
 
 print_head "Downloading frontend content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 status_check
 
 # shellcheck disable=SC2164
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log}
 
 print_head "Extracting the frontend content"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 status_check
 
 print_head "Copying the files to default location"
 # shellcheck disable=SC1083
 # shellcheck disable=SC2164
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 status_check
 
 print_head "Enabling Nginx"
-systemctl enable nginx
+systemctl enable nginx &>>${log}
 status_check
 
 print_head "Starting Nginx"
-systemctl start nginx
+systemctl start nginx &>>${log}
 status_check
