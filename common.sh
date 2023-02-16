1d8c21761327e9c1cf1c037c5ff22fffa85f6326
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 22:59:37 +0000

    Redis script

--- common.sh
+++ common.sh
@@ -16,69 +16,27 @@ print_head() {
   echo -e "\e[1;m $1 \e[0m"
 }
 
-component () {
+APP_PREREQ() {
 
-  print_head "Downloading NodeJS"
-  curl -sL https://rpm.nodesource.com/setup_lts.x &>>${log}
-  status_check
-
-  print_head "Installing NodeJS"
-  yum install nodejs -y &>>${log}
-  status_check
-
-  print_head "Adding Application user"
+  print_head "Add Application User"
+  id roboshop &>>${LOG}
   if [ $? -ne 0 ]; then
-  useradd roboshop &>>${log}
+    useradd roboshop &>>${LOG}
   fi
   status_check
 
-  mkdir -p /app &>>${log}
+  mkdir -p /app &>>${LOG}
 
-  print_head "Downloading Application content"
-  curl -L -o /tmp/{component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip &>>${log}
+  print_head "Downloading App content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
   status_check
 
-  print_head "Removing previous files, if any"
-  rm -rf /app/* &>>${log}
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${LOG}
   status_check
 
-  print_head "Extracting App content"
-  # shellcheck disable=SC2164
+  print_head "Extracting App Content"
   cd /app
-  unzip /tmp/{component}.zip &>>${log}
+  unzip /tmp/${component}.zip &>>${LOG}
   status_check
-
-  # shellcheck disable=SC2164
-  cd /app
-  status_check
-
-  print_head "Installing NodeJS dependencies"
-  npm install &>>${log}
-  status_check
-
-  print_head "Configuring {component} service file"
-  cp ${script_location}/files/{component}.service /etc/systemd/system/{component}.service &>>${log}
-  status_check
-
-  print_head "Reloading SystemD"
-  systemctl daemon-reload &>>${log}
-  status_check
-
-  print_head "Installing MongoDB client "
-  # We are installing the mongo client as it was already downloaded
-  yum install mongodb-org-shell -y &>>${log}
-  status_check
-
-  print_head "Enabling {component}"
-  systemctl enable {component} &>>${log}
-  status_check
-
-  print_head "Starting {component}"
-  systemctl start {component} &>>${log}
-  status_check
-
-  print_head "Loading Schema"
-  mongo --host localhost </app/schema/{component}.js &>>${log}
-  status_check
-
 }
