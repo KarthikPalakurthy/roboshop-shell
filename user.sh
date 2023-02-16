cc76a75104cb6843f6621333440adc161972a673
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 22:51:35 +0000

    Redis script

--- user.sh
+++ user.sh
@@ -1,18 +1,43 @@
 script_location=$(pwd)
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+print_head "Downloading configuration files"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log}
+status_code
+
 yum install nodejs -y
+status_code
+
+id useradd
+if [ $? -ne 0 ]; then
 useradd roboshop
-mkdir /app
+fi
+status_code
+
+mkdir -p /app
+status_code
+
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+status_code
+
 cd /app
 unzip /tmp/user.zip
+status_code
+
 cd /app
 npm install
+status_code
 
 cp "${script_location}"/files/user.service /etc/systemd/system/user.service
+status_code
+
 systemctl daemon-reload
+status_code
+
 systemctl enable user
+status_code
 systemctl start user
+status_code
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+status_code
+mongo --host localhost </app/schema/user.js
+status_code
