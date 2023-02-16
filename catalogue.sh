08cf3098ea5f8d4c8bb750c379eb7b1dc30fb57b
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 12:33:17 +0000

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -7,31 +7,31 @@ curl -sL https://rpm.nodesource.com/setup_lts.x &>>${log}
 status_check
 
 print_head "Installing NodeJS"
-yum install nodejs -y
+yum install nodejs -y &>>${log}
 status_check
 
 print_head "Adding user"
 if [ $? -ne 0 ]; then
-useradd roboshop
+useradd roboshop &>>${log}
 fi
 status_check
 
 print_head "making the new directory"
-mkdir -p /app
+mkdir -p /app &>>${log}
 status_check
 
 print_head "Downloading catalogue content"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 status_check
 
 print_head "Removing previous files, if any"
-rm -rf /app/*
+rm -rf /app/* &>>${log}
 status_check
 
 print_head "Extracting the content"
 # shellcheck disable=SC2164
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log}
 status_check
 
 # shellcheck disable=SC2164
@@ -39,29 +39,29 @@ cd /app
 status_check
 
 print_head "Installing NPM"
-npm install
+npm install &>>${log}
 status_check
 
 print_head "Moving the extracted files to the default location"
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 status_check
 
 print_head "Loading user"
-systemctl daemon-reload
+systemctl daemon-reload &>>${log}
 status_check
 
 print_head "Installing MongoDB client "
 # We are installing the mongo client as it was already downloaded
-yum install mongodb-org-shell -y
-mongo --host localhost </app/schema/catalogue.js
+yum install mongodb-org-shell -y &>>${log}
+mongo --host localhost </app/schema/catalogue.js &>>${log}
 status_check
 
 print_head "Enabling Catalogue"
-systemctl enable catalogue
+systemctl enable catalogue &>>${log}
 status_check
 
 print_head "Starting Catalogue"
-systemctl start catalogue
+systemctl start catalogue &>>${log}
 status_check
 
 
