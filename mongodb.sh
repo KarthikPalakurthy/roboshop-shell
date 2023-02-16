53e2a7b992e25548fc9f586dc70a4c466d3b4b62
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 12:15:18 +0000

    front end

--- mongodb.sh
+++ mongodb.sh
@@ -2,21 +2,21 @@ source common.sh
 script_location=$(pwd)
 
 print_head "Copying MongoDB repo files"
-cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo # We are copying the .repo file to the yum repo
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongodb.repo &>>${log} # We are copying the .repo file to the yum repo
 status_check
 
 print_head "Installing MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log}
 status_check
 
 print_head "Changing the location"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log}
 status_check
 
 print_head "Enabling MongoDB"
-systemctl enable mongod
+systemctl enable mongod &>>${log}
 status_check
 
 print_head "Starting MongoDB"
-systemctl restart mongod
+systemctl restart mongod &>>${log}
 status_check
