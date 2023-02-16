334b11a2d9a52389d5aac25d427748b1fa9c46fc
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-16 22:19:24 +0000

    Redis script

--- user.sh
+++ user.sh
@@ -2,15 +2,15 @@ source common.sh
 script_location=${pwd}
 
 echo -e "\e[1;m Downloading repo files\e[0"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 status_check
 
 echo -e "\e[1;m Installing NodeJS\e[0"
-yum install nodejs -y &>>{log}
+yum install nodejs -y &>>${log}
 
 echo -e "\e[1; Adding user\e[0"
 if [ $? -ne 0 ]; then
-useradd roboshop &>>{log}
+useradd roboshop &>>${log}
 fi
 
 mkdir -p /app
@@ -18,7 +18,7 @@ mkdir -p /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>{log}
 status_check
 
-rm -rf /app/* &>>{log}
+rm -rf /app/* &>>${log}
 status_check
 
 
@@ -32,7 +32,7 @@ cd /app
 npm install
 status_check
 
-cp ${script_location}/files/user.service /etc/systemd/system/user.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 status_check
 
 systemctl daemon-reload
