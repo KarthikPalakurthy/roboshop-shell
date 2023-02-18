162f14d97994acd992d31eedc46281acc8f4e18d
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-18 07:47:25 +0000

    Redis script

--- mysql.sh
+++ mysql.sh
@@ -4,6 +4,10 @@ dnf module disable mysql -y
 
 cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 
+mysql_secure_installation --set-root-pass RoboShop@1
+
+mysql -uroot -pRoboShop@1
+
 yum install mysql-community-server -y
 
 systemctl enable mysqld
