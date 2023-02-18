ae1183f8797f3e26159c081ad3b9f3c84fa4b7f1
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-18 07:35:39 +0000

    Redis script

---
+++ mysql.sh
@@ -0,0 +1,8 @@
+dnf module disable mysql -y
+
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+
+yum install mysql-community-server -y
+
+systemctl enable mysqld
+systemctl start mysqld
