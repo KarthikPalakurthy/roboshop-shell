3333b2acd2960323b0d8d5c62ed41903da9144db
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-18 09:38:26 +0000

    Redis script

--- mysql.sh
+++ mysql.sh
@@ -7,27 +7,27 @@ fi
 
 
 print_head "Disable MySQL Default Module"
-dnf module disable mysql -y &>>${LOG}
+dnf module disable mysql -y &>>${log}
 status_check
 
 print_head "Copy MySQL Repo file"
-cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
 status_check
 
 print_head "Install MySQL Server"
-yum install mysql-community-server -y  &>>${LOG}
+yum install mysql-community-server -y  &>>${log}
 status_check
 
 print_head "Enable MySQL"
-systemctl enable mysqld &>>${LOG}
+systemctl enable mysqld &>>${log}
 status_check
 
 print_head "Start MySQL"
-systemctl restart mysqld &>>${LOG}
+systemctl restart mysqld &>>${log}
 status_check
 
 #print_head "Reset Default Database Password"
-#mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+#mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${log}
 #if [ $? -eq 1 ]; then
 #  echo "Password is already changed"
 #fi
