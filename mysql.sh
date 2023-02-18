b8ce934e958daf82520424e8870486a7aa0093db
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-18 07:48:30 +0000

    Redis script

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,35 @@
-script_location=$(pwd)
+source common.sh
 
-dnf module disable mysql -y
+if [ -z "${root_mysql_password}" ]; then
+  echo "Variable root_mysql_password is missing"
+  exit 1
+fi
 
+
+print_head "Disable MySQL Default Module"
+dnf module disable mysql -y &>>${LOG}
+status_check
+
+print_head "Copy MySQL Repo file"
 cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+status_check
 
-mysql_secure_installation --set-root-pass RoboShop@1
+print_head "Install MySQL Server"
+yum install mysql-community-server -y  &>>${LOG}
+status_check
 
-mysql -uroot -pRoboShop@1
+print_head "Enable MySQL"
+systemctl enable mysqld &>>${LOG}
+status_check
 
-yum install mysql-community-server -y
+print_head "Start MySQL"
+systemctl restart mysqld &>>${LOG}
+status_check
 
-systemctl enable mysql
+print_head "Reset Default Database Password"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+if [ $? -eq 1 ]; then
+  echo "Password is already changed"
+fi
+status_check
 
-systemctl start mysql
