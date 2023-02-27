216f415a25fd9e9dddb500ede00a5cddad0c97f8
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-27 13:25:45 +0000

    practice

---
+++ Practice/common code.sh
@@ -0,0 +1,8 @@
+status_check() {
+
+if [ $? -eq 0 ]; then
+ echo -e "\e[1;32m Successful\e[0m"
+ else
+ echo -e "\e[1;31m Failure\e[0m"
+ fi
+}
--- Practice/frontend.sh
+++ Practice/frontend.sh
@@ -1,4 +1,6 @@
+source commoncode.sh
 yum install nginx -y >>${log}
+status_check
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
