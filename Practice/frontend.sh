b25660fac3ac3f5ae023fc8311aa977cd8ca22d1
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-27 13:17:13 +0000

    practice

--- Practice/frontend.sh
+++ Practice/frontend.sh
@@ -1,4 +1,4 @@
-yum install nginx -y >>&{log}
+yum install nginx -y >>${log}
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
