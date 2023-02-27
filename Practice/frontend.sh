3d416d8c581bcc014fd274af686b411038413fb2
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-02-27 18:23:20 +0000

    practice

--- Practice/frontend.sh
+++ Practice/frontend.sh
@@ -5,12 +5,24 @@ log=/tmp/roboshop.log
 echo -e "\e[1;m Installing Nginx\e[0m"
 
 yum install nginx -y >>${log}
-
 status_check
+
+echo -e "\e[1;m Removing Old files\e[0m"
 rm -rf /usr/share/nginx/html/*
+status_check
+
+echo -e "\e[1; Downloading Frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+status_check
+
+
+# shellcheck disable=SC2164
 cd /usr/share/nginx/html
+
+echo -e "\e[1; Extracting frontend content\e[0m"
 unzip /tmp/frontend.zip
+status_check
+
 cp /usr/share/nginx/html /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
